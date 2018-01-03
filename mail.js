d84d5a06575ca93be76fdd9c561d87bfa25ae2e7
lersh <lersh99@gmail.com>
2018-01-03 11:12:41 +0800

    增加发邮件功能

---
+++ mail.js
@@ -0,0 +1,31 @@
+'use strict';
+const nodemailer = require('nodemailer');
+
+var transport = nodemailer.createTransport({
+    host: 'smtp.163.com',
+    port: 994,
+    secure: true,
+    auth: {
+        user: 'lersh',
+        pass: 'ltfpltfp@_@'
+    }
+});
+var mailOptions = {
+    from: '"Lersh"<lersh@163.com>',
+    to: 'lersh@163.com',
+    subject: '测试下node发邮件',
+    text: '测试下node发邮件',
+    html: '<b>测试下node发邮件</b>'
+};
+
+transport.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        console.error(error);
+    }
+    else {
+        console.log('Message sent: %s', info.messageId);
+        // Preview only available when sending through an Ethereal account
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    }
+});
+
